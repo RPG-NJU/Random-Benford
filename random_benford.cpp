7ebf5d1ce3090ba13975c691a701dbd194dc36af
RPG <41836383+HELLORPG@users.noreply.github.com>
2019-04-28 18:02:02 -0700

    [init]

---
+++ random_benford.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+#define GRPDEBUG
+
+
+
+int main()
+{
+    cout << "RAND_MAX = " << RAND_MAX << endl;
+
+    return 0;
+}
